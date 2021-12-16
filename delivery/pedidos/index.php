1ab822caf6ba7670decc089e7ff8a1de20d7d91a
DeividAlmeida <mr.davialmeida@hotmail.com>
2021-12-16 18:24:00 -0300

    printme

--- delivery/pedidos/index.php
+++ delivery/pedidos/index.php
@@ -124,7 +124,46 @@
 			})        
 			return html.join('');        
 		}
+        function PrintMe(DivID) {
+            var disp_setting="toolbar=yes,location=no,";
+            disp_setting+="directories=yes,menubar=yes,";
+            disp_setting+="scrollbars=yes,width=450, height=600, left=100, top=25";
+            var content_vlue = document.getElementById(DivID).innerHTML;
+            var docprint=window.open("","",disp_setting);
+            docprint.document.open();
+            docprint.document.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"');
+            docprint.document.write('"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">');
+            docprint.document.write('<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">');
+            docprint.document.write('<head><title>COMANDA BALCAO</title>');
+            docprint.document.write('<style type="text/css">body{ margin:0px;');
+            docprint.document.write('font-family:verdana,Arial;color:#000;');
+            docprint.document.write('font-family:Verdana, Geneva, sans-serif; font-size:12px;}');
+            docprint.document.write('a{color:#000;text-decoration:none;} </style>');
+            docprint.document.write('</head><body onLoad="self.print()">');
+            docprint.document.write(content_vlue);
+            docprint.document.write('</body></html>');
+            docprint.document.close();
+            docprint.focus();
+        }
+        function PrintMe2(DivID) {
+            var disp_setting="toolbar=yes,location=no,";
+            disp_setting+="directories=yes,menubar=yes,";
+            disp_setting+="scrollbars=yes,width=450, height=600, left=100, top=25";
+            var content_vlue = document.getElementsByClassName(DivID)[0].innerHTML;
+            var docprint=window.open("","",disp_setting);
+            docprint.document.open();
+            docprint.document.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"');
+            docprint.document.write('"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">');
+            docprint.document.write('<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">');
+            docprint.document.write('<head><title>COMANDA MESA</title>');
+            docprint.document.write('<style type="text/css">body{ margin:0px;');
+            docprint.document.write('font-family:verdana,Arial;color:#000;');
+            docprint.document.write('font-family:Verdana, Geneva, sans-serif; font-size:12px;}');
+            docprint.document.write('a{color:#000;text-decoration:none;} </style>');
+            docprint.document.write('</head><body onLoad="self.print()"><center>');
+            docprint.document.write(content_vlue);
+            docprint.document.write('</center></body></html>');
+            docprint.document.close();
+            docprint.focus();
+        }
     </script>
-
-  
-</script>
